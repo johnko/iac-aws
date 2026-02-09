8c59981a71fe618f19628abc80d7c763016b1cfb
johnko <279736+johnko@users.noreply.github.com>
2026-02-09 02:55:16 -0500

    import EC2_NoPublicSharingSnapshot_POLICY

---
+++ aws/org1/management/organizations_policy_EC2_NoPublicSharingSnapshot_POLICY.tf
@@ -0,0 +1,42 @@
+variable "aws_EC2_NoPublicSharingSnapshot_POLICY" {
+  type        = string
+  description = "Policy ID, eg. p-123"
+}
+import {
+  to = aws_organizations_policy.EC2_NoPublicSharingSnapshot_POLICY
+  id = var.aws_EC2_NoPublicSharingSnapshot_POLICY
+}
+
+resource "aws_organizations_policy" "EC2_NoPublicSharingSnapshot_POLICY" {
+
+  name = "NoPublicSharingSnapshot"
+  type = "DECLARATIVE_POLICY_EC2"
+
+  content = jsonencode({
+    "ec2_attributes" : {
+      "snapshot_block_public_access" : {
+        "state" : {
+          "@@assign" : "block_all_sharing"
+        }
+      }
+    }
+  })
+
+  tags = {
+    "iacdeployer" = "awsconsole"
+  }
+
+}
+
+import {
+  to = aws_organizations_policy_attachment.EC2_NoPublicSharingSnapshot_POLICY_root
+  identity = {
+    policy_id = var.aws_EC2_NoPublicSharingSnapshot_POLICY
+    target_id = aws_organizations_organization.org.roots[0].id
+  }
+}
+
+resource "aws_organizations_policy_attachment" "EC2_NoPublicSharingSnapshot_POLICY_root" {
+  policy_id = aws_organizations_policy.EC2_NoPublicSharingSnapshot_POLICY.id
+  target_id = aws_organizations_organization.org.roots[0].id
+}
