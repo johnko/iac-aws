be04379c5a0324f084ddc9348ed2bfcc1818d0db
johnko <279736+johnko@users.noreply.github.com>
2026-02-09 03:21:53 -0500

    import TAG_POLICY

---
+++ aws/org1/management/organizations_policy_TAG_iacdeployerEnumValues_POLICY.tf
@@ -0,0 +1,50 @@
+variable "aws_TAG_iacdeployerEnumValues_POLICY" {
+  type        = string
+  description = "Policy ID, eg. p-123"
+}
+import {
+  to = aws_organizations_policy.TAG_iacdeployerEnumValues_POLICY
+  id = var.aws_TAG_iacdeployerEnumValues_POLICY
+}
+
+resource "aws_organizations_policy" "TAG_iacdeployerEnumValues_POLICY" {
+
+  name        = "iacdeployerEnumValues"
+  description = "Limit iacdeployer values"
+  type        = "TAG_POLICY"
+
+  content = jsonencode({
+    "tags" : {
+      "iacdeployer" : {
+        "tag_value" : {
+          "@@assign" : [
+            "awsautomatic",
+            "awscli",
+            "awsconsole",
+            "cloudformation",
+            "pulumi",
+            "terraform"
+          ]
+        }
+      }
+    }
+  })
+
+  tags = {
+    "iacdeployer" = "awsconsole"
+  }
+
+}
+
+import {
+  to = aws_organizations_policy_attachment.TAG_iacdeployerEnumValues_POLICY_root
+  identity = {
+    policy_id = var.aws_TAG_iacdeployerEnumValues_POLICY
+    target_id = aws_organizations_organization.org.roots[0].id
+  }
+}
+
+resource "aws_organizations_policy_attachment" "TAG_iacdeployerEnumValues_POLICY_root" {
+  policy_id = aws_organizations_policy.TAG_iacdeployerEnumValues_POLICY.id
+  target_id = aws_organizations_organization.org.roots[0].id
+}
