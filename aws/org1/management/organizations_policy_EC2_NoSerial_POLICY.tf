c07adba3c188418662d2c9b80619a5f49b10c3fb
johnko <279736+johnko@users.noreply.github.com>
2026-02-09 03:00:39 -0500

    import EC2_NoSerial_POLICY

---
+++ aws/org1/management/organizations_policy_EC2_NoSerial_POLICY.tf
@@ -0,0 +1,42 @@
+variable "aws_EC2_NoSerial_POLICY" {
+  type        = string
+  description = "Policy ID, eg. p-123"
+}
+import {
+  to = aws_organizations_policy.EC2_NoSerial_POLICY
+  id = var.aws_EC2_NoSerial_POLICY
+}
+
+resource "aws_organizations_policy" "EC2_NoSerial_POLICY" {
+
+  name = "NoSerial"
+  type = "DECLARATIVE_POLICY_EC2"
+
+  content = jsonencode({
+    "ec2_attributes" : {
+      "serial_console_access" : {
+        "status" : {
+          "@@assign" : "disabled"
+        }
+      }
+    }
+  })
+
+  tags = {
+    "iacdeployer" = "awsconsole"
+  }
+
+}
+
+import {
+  to = aws_organizations_policy_attachment.EC2_NoSerial_POLICY_root
+  identity = {
+    policy_id = var.aws_EC2_NoSerial_POLICY
+    target_id = aws_organizations_organization.org.roots[0].id
+  }
+}
+
+resource "aws_organizations_policy_attachment" "EC2_NoSerial_POLICY_root" {
+  policy_id = aws_organizations_policy.EC2_NoSerial_POLICY.id
+  target_id = aws_organizations_organization.org.roots[0].id
+}
