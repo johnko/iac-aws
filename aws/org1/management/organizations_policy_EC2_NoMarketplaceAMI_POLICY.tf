b7dca8da836291da0f04084ae64d86f93d4a81fc
johnko <279736+johnko@users.noreply.github.com>
2026-02-09 02:47:06 -0500

    import EC2_NoMarketplaceAMI_POLICY

---
+++ aws/org1/management/organizations_policy_EC2_NoMarketplaceAMI_POLICY.tf
@@ -0,0 +1,52 @@
+variable "aws_EC2_NoMarketplaceAMI_POLICY" {
+  type        = string
+  description = "Policy ID, eg. p-123"
+}
+import {
+  to = aws_organizations_policy.EC2_NoMarketplaceAMI_POLICY
+  id = var.aws_EC2_NoMarketplaceAMI_POLICY
+}
+
+resource "aws_organizations_policy" "EC2_NoMarketplaceAMI_POLICY" {
+
+  name = "NoMarketplaceAMI"
+  type = "DECLARATIVE_POLICY_EC2"
+
+  content = jsonencode({
+    "ec2_attributes" : {
+      "allowed_images_settings" : {
+        "state" : {
+          "@@assign" : "enabled"
+        },
+        "image_criteria" : {
+          "criteria_1" : {
+            "allowed_image_providers" : {
+              "@@assign" : [
+                "amazon",
+                "aws_backup_vault"
+              ]
+            }
+          }
+        }
+      }
+    }
+  })
+
+  tags = {
+    "iacdeployer" = "awsconsole"
+  }
+
+}
+
+import {
+  to = aws_organizations_policy_attachment.EC2_NoMarketplaceAMI_POLICY_root
+  identity = {
+    policy_id = var.aws_EC2_NoMarketplaceAMI_POLICY
+    target_id = aws_organizations_organization.org.roots[0].id
+  }
+}
+
+resource "aws_organizations_policy_attachment" "EC2_NoMarketplaceAMI_POLICY_root" {
+  policy_id = aws_organizations_policy.EC2_NoMarketplaceAMI_POLICY.id
+  target_id = aws_organizations_organization.org.roots[0].id
+}
