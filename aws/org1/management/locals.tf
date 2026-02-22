6c18e8dbcb56262f90ec653f3769ec5968579e4f
johnko <279736+johnko@users.noreply.github.com>
2026-02-22 05:50:29 -0500

    add us-west-2 for bedrock cross-region inference profile

--- aws/org1/management/locals.tf
+++ aws/org1/management/locals.tf
@@ -1,8 +1,10 @@
 locals {
-  governedRegions = [ # List of regions to govern, order matters to prevent change
+  governedRegions = [ # List of regions to govern
+    ##### ORDER MATTERS TO PREVENT CHANGE
     "ca-central-1",
-    "us-east-2",
+    "us-east-2", # bedrock cross-region inference profile
     "us-east-1",
-    "ca-west-1"
+    "ca-west-1",
+    "us-west-2", # bedrock cross-region inference profile
   ]
 }
