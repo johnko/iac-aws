0823173b680922ab5b900e0c3feb2adc07dda6fb
johnko <279736+johnko@users.noreply.github.com>
2026-02-23 01:48:11 -0500

    resource_explorer_org should only be in management

--- aws/org1/shared_resource_explorer_account.tf
+++ aws/org1/shared_resource_explorer_account.tf
@@ -9,3 +9,15 @@ resource "aws_resourceexplorer2_index" "account" {
   region = each.value.region
   type   = each.value.type
 }
+
+resource "aws_resourceexplorer2_index" "org" {
+  for_each = {
+    for region in local.unusedRegions : region => {
+      "region" : region,
+      "type" : region == "ca-central-1" ? "AGGREGATOR" : "LOCAL"
+    }
+  }
+
+  region = each.value.region
+  type   = each.value.type
+}
