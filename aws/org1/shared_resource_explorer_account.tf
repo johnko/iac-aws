f17c39bab991949df5fee0a673df15ad9cd21b54
johnko <279736+johnko@users.noreply.github.com>
2026-02-24 02:49:36 -0500

    filter resource index for unusedRegions with resourceExplorerAccountWithoutUnusedRegions

--- aws/org1/shared_resource_explorer_account.tf
+++ aws/org1/shared_resource_explorer_account.tf
@@ -10,12 +10,21 @@ resource "aws_resourceexplorer2_index" "account" {
   type   = each.value.type
 }
 
+data "aws_caller_identity" "current" {}
+
+locals {
+  resourceExplorerAccountWithoutUnusedRegions = [
+    var.aws_account_id_security_aggregator,
+    var.aws_account_id_security_cloudtrail,
+  ]
+}
+
 resource "aws_resourceexplorer2_index" "unusedRegions" {
   for_each = {
     for region in local.unusedRegions : region => {
       "region" : region,
       "type" : region == "ca-central-1" ? "AGGREGATOR" : "LOCAL"
-    }
+    } if contains(local.resourceExplorerAccountWithoutUnusedRegions, data.aws_caller_identity.current.account_id)
   }
 
   region = each.value.region
