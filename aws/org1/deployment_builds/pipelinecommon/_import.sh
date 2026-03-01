f99387cfbe0910b2425f03630f2bcfd38f689ac5
johnko <279736+johnko@users.noreply.github.com>
2026-03-01 13:35:17 -0500

    shellcheck

--- aws/org1/deployment_builds/pipelinecommon/_import.sh
+++ aws/org1/deployment_builds/pipelinecommon/_import.sh
@@ -8,10 +8,10 @@ if [[ ! -e "$TERRAFORM_FILENAME" ]]; then
 fi
 
 for i in cacentral1 useast2; do
-  if aws s3api get-bucket-location --bucket codepipeline-${TF_VAR_aws_account_id_deployment_builds}-$i --no-cli-pager ; then
-    maybe_filename=$(aws s3api list-objects-v2 --bucket codepipeline-${TF_VAR_aws_account_id_deployment_builds}-$i --prefix "$TERRAFORM_FILENAME" --output text --no-cli-pager --query 'Contents[].Key')
+  if aws s3api get-bucket-location --bucket "codepipeline-${TF_VAR_aws_account_id_deployment_builds}-$i" --no-cli-pager ; then
+    maybe_filename=$(aws s3api list-objects-v2 --bucket "codepipeline-${TF_VAR_aws_account_id_deployment_builds}-$i" --prefix "$TERRAFORM_FILENAME" --output text --no-cli-pager --query 'Contents[].Key')
     if [[ "$maybe_filename" != "$TERRAFORM_FILENAME" ]]; then
-      aws s3 cp "$TERRAFORM_FILENAME" s3://codepipeline-${TF_VAR_aws_account_id_deployment_builds}-$i/
+      aws s3 cp "$TERRAFORM_FILENAME" "s3://codepipeline-${TF_VAR_aws_account_id_deployment_builds}-$i/"
     fi
   fi
 done
--- aws/org1/install.sh
+++ aws/org1/install.sh
@@ -8,12 +8,12 @@ if [[ -e .envrc ]]; then
   source .envrc
 fi
 
-if echo $EXECUTOR_TYPE | grep LAMBDA; then
+if echo "$EXECUTOR_TYPE" | grep LAMBDA; then
 
   TERRAFORM_FILENAME="terraform_${TERRAFORM_VERSION}_linux_amd64.zip"
-  i=$(echo $AWS_REGION | tr -d '-')
-  if aws s3api get-bucket-location --bucket codepipeline-${TF_VAR_aws_account_id_deployment_builds}-$i --no-cli-pager ; then
-    maybe_filename=$(aws s3api list-objects-v2 --bucket codepipeline-${TF_VAR_aws_account_id_deployment_builds}-$i --prefix "$TERRAFORM_FILENAME" --output text --no-cli-pager --query 'Contents[].Key')
+  i=$(echo "$AWS_REGION" | tr -d '-')
+  if aws s3api get-bucket-location --bucket "codepipeline-${TF_VAR_aws_account_id_deployment_builds}-$i" --no-cli-pager ; then
+    maybe_filename=$(aws s3api list-objects-v2 --bucket "codepipeline-${TF_VAR_aws_account_id_deployment_builds}-$i" --prefix "$TERRAFORM_FILENAME" --output text --no-cli-pager --query 'Contents[].Key')
     if [[ "$maybe_filename" == "$TERRAFORM_FILENAME" ]]; then
       aws s3 cp "s3://codepipeline-${TF_VAR_aws_account_id_deployment_builds}-$i/$TERRAFORM_FILENAME" ./
     fi
