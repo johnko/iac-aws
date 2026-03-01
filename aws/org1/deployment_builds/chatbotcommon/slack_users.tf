ae5dac0af8471ae6c66d031c1c07810679c96f31
johnko <279736+johnko@users.noreply.github.com>
2026-03-01 03:40:47 -0500

    allow inline_policy1 overrides

---
+++ aws/org1/deployment_builds/chatbotcommon/slack_users.tf
@@ -0,0 +1,67 @@
+locals {
+  slack_user_roles = {
+    # maybe use GitHub usernames
+    "johnko" = {}
+  }
+
+  default_inline_policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Action" : [
+          "cloudwatch:Describe*",
+          "cloudwatch:Get*",
+          "cloudwatch:List*"
+        ],
+        "Effect" : "Allow",
+        "Resource" : "*",
+        "Sid" : "AWSChatbotNotificationsOnlyPolicy"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role" "chatbot_user" {
+  for_each = local.slack_user_roles
+
+  name = "UserChatbotRole-${each.key}"
+
+  assume_role_policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Effect" : "Allow",
+        "Principal" : {
+          "Service" : "chatbot.amazonaws.com"
+        },
+        "Action" : "sts:AssumeRole"
+      }
+    ]
+  })
+
+}
+
+resource "aws_iam_role_policy_attachments_exclusive" "example" {
+  for_each = local.slack_user_roles
+
+  role_name = aws_iam_role.chatbot_user[each.key].name
+  policy_arns = [
+    "arn:aws:iam::aws:policy/AmazonQDeveloperAccess"
+  ]
+}
+
+resource "aws_iam_role_policy" "inline_policy1" {
+  for_each = local.slack_user_roles
+
+  name = "inline_policy1"
+  role = aws_iam_role.chatbot_user[each.key].id
+
+  policy = try(length(each.value.inline_policy1) > 0, false) ? each.value.inline_policy1 : local.default_inline_policy
+}
+
+resource "aws_iam_role_policies_exclusive" "chatbot_user_inline" {
+  for_each = local.slack_user_roles
+
+  role_name    = aws_iam_role.chatbot_user[each.key].name
+  policy_names = [resource.aws_iam_role_policy.inline_policy1[each.key].name]
+}
