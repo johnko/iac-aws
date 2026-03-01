c757ea9b706279896b394813f57250ac14ad54be
johnko <279736+johnko@users.noreply.github.com>
2026-03-01 15:02:57 -0500

    only upload tf zip if not in CI

--- aws/org1/deployment_builds/pipelinecommon/_import.sh
+++ aws/org1/deployment_builds/pipelinecommon/_import.sh
@@ -3,15 +3,15 @@ set -euxo pipefail
 
 TERRAFORM_FILENAME="terraform_${TERRAFORM_VERSION}_linux_amd64.zip"
 
-if [[ ! -e $TERRAFORM_FILENAME ]]; then
+if [[ ! -e $TERRAFORM_FILENAME ]] && [[ $CI != true ]]; then
   curl -LO "https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/${TERRAFORM_FILENAME}"
-fi
 
-for i in cacentral1 useast2; do
-  if aws s3api get-bucket-location --bucket "codepipeline-${TF_VAR_aws_account_id_deployment_builds}-$i" --no-cli-pager; then
-    maybe_filename=$(aws s3api list-objects-v2 --bucket "codepipeline-${TF_VAR_aws_account_id_deployment_builds}-$i" --prefix "$TERRAFORM_FILENAME" --output text --no-cli-pager --query 'Contents[].Key')
-    if [[ $maybe_filename != "$TERRAFORM_FILENAME" ]]; then
-      aws s3 cp "$TERRAFORM_FILENAME" "s3://codepipeline-${TF_VAR_aws_account_id_deployment_builds}-$i/"
+  for i in cacentral1 useast2; do
+    if aws s3api get-bucket-location --bucket "codepipeline-${TF_VAR_aws_account_id_deployment_builds}-$i" --no-cli-pager; then
+      maybe_filename=$(aws s3api list-objects-v2 --bucket "codepipeline-${TF_VAR_aws_account_id_deployment_builds}-$i" --prefix "$TERRAFORM_FILENAME" --output text --no-cli-pager --query 'Contents[].Key')
+      if [[ $maybe_filename != "$TERRAFORM_FILENAME" ]]; then
+        aws s3 cp "$TERRAFORM_FILENAME" "s3://codepipeline-${TF_VAR_aws_account_id_deployment_builds}-$i/"
+      fi
     fi
-  fi
-done
+  done
+fi
--- aws/org1/install.sh
+++ aws/org1/install.sh
@@ -8,28 +8,31 @@ if [[ -e .envrc ]]; then
   source .envrc
 fi
 
-if echo "$EXECUTOR_TYPE" | grep LAMBDA; then
+# verbose to see install steps
+set -x
 
-  TERRAFORM_FILENAME="terraform_${TERRAFORM_VERSION}_linux_amd64.zip"
-  i=$(echo "$AWS_REGION" | tr -d '-')
-  if aws s3api get-bucket-location --bucket "codepipeline-${TF_VAR_aws_account_id_deployment_builds}-$i" --no-cli-pager; then
-    maybe_filename=$(aws s3api list-objects-v2 --bucket "codepipeline-${TF_VAR_aws_account_id_deployment_builds}-$i" --prefix "$TERRAFORM_FILENAME" --output text --no-cli-pager --query 'Contents[].Key')
-    if [[ $maybe_filename == "$TERRAFORM_FILENAME" ]]; then
-      aws s3 cp "s3://codepipeline-${TF_VAR_aws_account_id_deployment_builds}-$i/$TERRAFORM_FILENAME" ./
-      unzip "$TERRAFORM_FILENAME"
-      mkdir -p ~/bin
-      mv terraform ~/bin/
-      export PATH=$PATH:~/bin
-    fi
+TERRAFORM_FILENAME="terraform_${TERRAFORM_VERSION}_linux_amd64.zip"
+i=$(echo "$AWS_REGION" | tr -d '-')
+if aws s3api get-bucket-location --bucket "codepipeline-${TF_VAR_aws_account_id_deployment_builds}-$i" --no-cli-pager; then
+  maybe_filename=$(aws s3api list-objects-v2 --bucket "codepipeline-${TF_VAR_aws_account_id_deployment_builds}-$i" --prefix "$TERRAFORM_FILENAME" --output text --no-cli-pager --query 'Contents[].Key')
+  if [[ $maybe_filename == "$TERRAFORM_FILENAME" ]]; then
+    aws s3 cp "s3://codepipeline-${TF_VAR_aws_account_id_deployment_builds}-$i/$TERRAFORM_FILENAME" ./ --quiet
+    unzip "$TERRAFORM_FILENAME"
+    mkdir -p ~/bin
+    mv terraform ~/bin/
+    export PATH=$PATH:~/bin
   fi
+fi
 
-else
-
-  export release=AmazonLinux
-  dnf install -y dnf-plugins-core
-  dnf config-manager --add-repo https://rpm.releases.hashicorp.com/$release/hashicorp.repo
-  dnf install -y terraform
-
+if ! echo "$EXECUTOR_TYPE" | grep LAMBDA; then
+  if ! terraform version &>/dev/null; then
+    export release=AmazonLinux
+    dnf install -y dnf-plugins-core
+    dnf config-manager --add-repo https://rpm.releases.hashicorp.com/$release/hashicorp.repo
+    dnf install -y terraform
+  fi
 fi
 
 terraform version
+
+set +x
