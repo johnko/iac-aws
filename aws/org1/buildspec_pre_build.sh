915db488918e5afe09041ca2892af1011fe55225
johnko <279736+johnko@users.noreply.github.com>
2026-03-01 23:26:05 -0500

    comment

--- aws/org1/buildspec_pre_build.sh
+++ aws/org1/buildspec_pre_build.sh
@@ -2,7 +2,7 @@
 set -euxo pipefail
 
 # Try to disable the current pipeline's stage transition to prevent the TerraformPlan stage to be superseded by the next queued execution
-# Will need to disable via the buildspec_post_build.sh
+# Will need to re-enable stage transition via the buildspec_post_build.sh
 set +e
 aws codepipeline disable-stage-transition \
   --pipeline-name "$CODEPIPELINE_NAME" \
