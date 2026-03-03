9f292e86dbf7486cccfb2ee8da7eef75208b477d
johnko <279736+johnko@users.noreply.github.com>
2026-03-03 00:16:28 -0500

    add s3:GetBucketPolicy to crossaccount

--- aws/org1/foundation_crossaccount_pipeline_role_plan.tf
+++ aws/org1/foundation_crossaccount_pipeline_role_plan.tf
@@ -39,13 +39,14 @@ locals {
             },
             "Action" : [
               "s3:DeleteObject",
-              "s3:PutObject",
-              "s3:GetObject",
-              "s3:GetObjectVersion",
-              "s3:GetBucketVersioning",
               "s3:GetBucketAcl",
               "s3:GetBucketLocation",
+              "s3:GetBucketPolicy",
+              "s3:GetBucketVersioning",
+              "s3:GetObject",
+              "s3:GetObjectVersion",
               "s3:ListBucket",
+              "s3:PutObject",
             ],
             "Resource" : [
               "arn:aws:s3:::tfstate-111122223333",
