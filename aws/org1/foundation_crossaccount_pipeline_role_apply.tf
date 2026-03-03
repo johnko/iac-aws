924ec07c15ef3f85fc2df26cc616d259c1c55afa
johnko <279736+johnko@users.noreply.github.com>
2026-03-03 00:17:27 -0500

    fix cross accont apply permissions

--- aws/org1/foundation_crossaccount_pipeline_role_apply.tf
+++ aws/org1/foundation_crossaccount_pipeline_role_apply.tf
@@ -26,13 +26,98 @@ locals {
   crossaccount_inline_policies_apply = {
     # This role starts with ViewOnly to avoid reading sensitive data like Secrets or S3
     # Here, be very selective what permissions are granted
-    CrossAccountInlinePolicy1 = {
-      policy = jsonencode({
+    ReadSSMParamWriteTerraformState = {
+      enabled_aws_account_ids = keys(local.all_aws_account_ids)
+      policy_template = jsonencode({
+        "Version" : "2012-10-17",
+        "Statement" : [
+          {
+            # Allow write to tfstate bucket
+            "Condition" : {
+              "StringEquals" : {
+                "aws:ResourceAccount" : "111122223333"
+              }
+            },
+            "Action" : [
+              "s3:DeleteObject",
+              "s3:GetBucketAcl",
+              "s3:GetBucketLocation",
+              "s3:GetBucketPolicy",
+              "s3:GetBucketVersioning",
+              "s3:GetObject",
+              "s3:GetObjectVersion",
+              "s3:ListBucket",
+              "s3:PutObject",
+            ],
+            "Resource" : [
+              "arn:aws:s3:::tfstate-111122223333",
+              "arn:aws:s3:::tfstate-111122223333/*"
+            ],
+            "Effect" : "Allow"
+          },
+          {
+            # Allow to get SSM Parameter
+            "Condition" : {
+              "StringEquals" : {
+                "aws:ResourceAccount" : "111122223333"
+              }
+            },
+            "Action" : [
+              "ssm:GetParameters",
+            ],
+            "Resource" : [
+              "arn:aws:ssm:*:111122223333:parameter/TF_VAR_*"
+            ],
+            "Effect" : "Allow"
+          },
+          {
+            # Read tag policy
+            "Action" : [
+              "tag:ListRequiredTags",
+            ],
+            "Resource" : "*",
+            "Effect" : "Allow"
+          },
+        ]
+      })
+    }
+    ReadPermissions1 = {
+      enabled_aws_account_ids = keys(local.all_aws_account_ids)
+      policy_template = jsonencode({
         "Version" : "2012-10-17",
         "Statement" : [
           {
             "Action" : [
-              "iam:GetRole"
+              "iam:GetAccessKeyLastUsed",
+              "iam:GetAccountAuthorizationDetails",
+              "iam:GetAccountName",
+              "iam:GetAccountPasswordPolicy",
+              "iam:GetAccountSummary",
+              "iam:GetCloudFrontPublicKey",
+              "iam:GetContextKeysForCustomPolicy",
+              "iam:GetContextKeysForPrincipalPolicy",
+              "iam:GetCredentialReport",
+              "iam:GetDelegationRequest",
+              "iam:GetGroup",
+              "iam:GetGroupPolicy",
+              "iam:GetHumanReadableSummary",
+              "iam:GetInstanceProfile",
+              "iam:GetLoginProfile",
+              "iam:GetMFADevice",
+              "iam:GetOpenIDConnectProvider",
+              "iam:GetOrganizationsAccessReport",
+              "iam:GetOutboundWebIdentityFederationInfo",
+              "iam:GetPolicy",
+              "iam:GetPolicyVersion",
+              "iam:GetRole",
+              "iam:GetRolePolicy",
+              "iam:GetSAMLProvider",
+              "iam:GetSSHPublicKey",
+              "iam:GetServiceLastAccessedDetails",
+              "iam:GetServiceLastAccessedDetailsWithEntities",
+              "iam:GetServiceLinkedRoleDeletionStatus",
+              "iam:GetUser",
+              "iam:GetUserPolicy",
             ],
             "Resource" : "*",
             "Effect" : "Allow"
