40737a1fee51045dcf36f2df0f204196bf7c6489
johnko <279736+johnko@users.noreply.github.com>
2026-03-02 23:06:52 -0500

    add some permissions to crossaccount plan role

--- aws/org1/foundation_crossaccount_pipeline_role_plan.tf
+++ aws/org1/foundation_crossaccount_pipeline_role_plan.tf
@@ -14,7 +14,6 @@ resource "aws_iam_role" "crossaccount_terraform_plan" {
   })
 }
 
-
 resource "aws_iam_role_policy_attachments_exclusive" "crossaccount_terraform_plan" {
   role_name = aws_iam_role.crossaccount_terraform_plan.name
   policy_arns = [
@@ -26,13 +25,97 @@ locals {
   crossaccount_inline_policies_plan = {
     # This role starts with ViewOnly to avoid reading sensitive data like Secrets or S3
     # Here, be very selective what permissions are granted
-    CrossAccountInlinePolicy1 = {
-      policy = jsonencode({
+    ReadSSMParamWriteTerraformState = {
+      enabled_aws_account_ids = keys(local.all_aws_account_ids)
+      policy_template = jsonencode({
+        "Version" : "2012-10-17",
+        "Statement" : [
+          {
+            # Allow write to tfstate bucket
+            "Condition" : {
+              "StringEquals" : {
+                "aws:ResourceAccount" : "111122223333"
+              }
+            },
+            "Action" : [
+              "s3:DeleteObject",
+              "s3:PutObject",
+              "s3:GetObject",
+              "s3:GetObjectVersion",
+              "s3:GetBucketVersioning",
+              "s3:GetBucketAcl",
+              "s3:GetBucketLocation",
+              "s3:ListBucket",
+            ],
+            "Resource" : [
+              "arn:aws:s3:::tfstate-111122223333",
+              "arn:aws:s3:::tfstate-111122223333/*"
+            ],
+            "Effect" : "Allow"
+          },
+          {
+            # Allow to get SSM Parameter
+            "Condition" : {
+              "StringEquals" : {
+                "aws:ResourceAccount" : "111122223333"
+              }
+            },
+            "Action" : [
+              "ssm:GetParameters",
+            ],
+            "Resource" : [
+              "arn:aws:ssm:*:111122223333:parameter/TF_VAR_*"
+            ],
+            "Effect" : "Allow"
+          },
+          {
+            # Read tag policy
+            "Action" : [
+              "tag:ListRequiredTags",
+            ],
+            "Resource" : "*",
+            "Effect" : "Allow"
+          },
+        ]
+      })
+    }
+    ReadPermissions1 = {
+      enabled_aws_account_ids = keys(local.all_aws_account_ids)
+      policy_template = jsonencode({
         "Version" : "2012-10-17",
         "Statement" : [
           {
             "Action" : [
-              "iam:GetRole"
+              "iam:GetAccessKeyLastUsed",
+              "iam:GetAccountAuthorizationDetails",
+              "iam:GetAccountName",
+              "iam:GetAccountPasswordPolicy",
+              "iam:GetAccountSummary",
+              "iam:GetCloudFrontPublicKey",
+              "iam:GetContextKeysForCustomPolicy",
+              "iam:GetContextKeysForPrincipalPolicy",
+              "iam:GetCredentialReport",
+              "iam:GetDelegationRequest",
+              "iam:GetGroup",
+              "iam:GetGroupPolicy",
+              "iam:GetHumanReadableSummary",
+              "iam:GetInstanceProfile",
+              "iam:GetLoginProfile",
+              "iam:GetMFADevice",
+              "iam:GetOpenIDConnectProvider",
+              "iam:GetOrganizationsAccessReport",
+              "iam:GetOutboundWebIdentityFederationInfo",
+              "iam:GetPolicy",
+              "iam:GetPolicyVersion",
+              "iam:GetRole",
+              "iam:GetRolePolicy",
+              "iam:GetSAMLProvider",
+              "iam:GetSSHPublicKey",
+              "iam:GetServiceLastAccessedDetails",
+              "iam:GetServiceLastAccessedDetailsWithEntities",
+              "iam:GetServiceLinkedRoleDeletionStatus",
+              "iam:GetUser",
+              "iam:GetUserPolicy",
             ],
             "Resource" : "*",
             "Effect" : "Allow"
@@ -44,11 +127,14 @@ locals {
 }
 
 resource "aws_iam_role_policy" "crossaccount_terraform_plan" {
-  for_each = local.crossaccount_inline_policies_plan
+  for_each = {
+    for k, v in local.crossaccount_inline_policies_plan :
+    k => v if contains(v.enabled_aws_account_ids, data.aws_caller_identity.current.account_id)
+  }
 
   name   = each.key
   role   = aws_iam_role.crossaccount_terraform_plan.id
-  policy = each.value.policy
+  policy = replace(each.value.policy_template, "111122223333", data.aws_caller_identity.current.account_id)
 }
 
 resource "aws_iam_role_policies_exclusive" "crossaccount_terraform_plan" {
