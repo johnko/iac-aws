4fe27bce54b9431d2a8c71e10d54160d880492d7
johnko <279736+johnko@users.noreply.github.com>
2026-03-03 08:02:15 -0500

    fix

--- aws/org1/buildspec_post_build.sh
+++ aws/org1/buildspec_post_build.sh
@@ -12,8 +12,8 @@ post_plan_to_slack() {
   if [[ -e $TF_TMP_LOG ]] && [[ $TF_TMP_LOG == *plan* ]]; then
     set +ux
     curl -X POST -H 'Content-type: application/json; charset=utf-8' \
-      --data '{"text":"Plan for pipeline '"$CODEPIPELINE_NAME"' in region '"$AWS_REGION"'\n```'"$($(cat "$TF_TMP_LOG" |
-        awk '/^No changes|^Terraform used the selected providers/,/NON MATCHING PATTERN TO GET ALL OUTPUT TO THE END/' | head -c 500))"'...```"}' \
+      --data '{"text":"Plan for pipeline '"$CODEPIPELINE_NAME"' in region '"$AWS_REGION"'\n```'"$(cat "$TF_TMP_LOG" |
+        awk '/^No changes|^Terraform used the selected providers/,/NON MATCHING PATTERN TO GET ALL OUTPUT TO THE END/' | head -c 500)"'...```"}' \
       $TF_VAR_PLAN_SLACK_WEBHOOK_URL
   fi
 }
