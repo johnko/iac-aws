7c61dd77e2c96bea5fa8b5b97611c63c6563b0a5
johnko <279736+johnko@users.noreply.github.com>
2026-03-04 23:44:25 -0500

    also re-enable if apply

--- aws/org1/buildspec_post_build.sh
+++ aws/org1/buildspec_post_build.sh
@@ -2,17 +2,25 @@
 set -euxo pipefail
 
 set +eu
+enable_stage_transition() {
+  # Try to enable the current pipeline's stage transition if error or No changes to allow the pipeline to resume after pausing for current execution
+  aws codepipeline enable-stage-transition \
+    --pipeline-name "$CODEPIPELINE_NAME" \
+    --stage-name Plan \
+    --transition-type Inbound
+}
+
 # don't enable on TF_PLAN_EXIT_CODE 2 because it will enable stage transition for current pipeline, which will cause plan to be overwritten by next queued execution
 if [[ -n $TF_PLAN_EXIT_CODE ]] && [[ $TF_PLAN_EXIT_CODE != 2 ]]; then
   # TF_PLAN_EXIT_CODE
   # 0 = Succeeded with empty diff (no changes), need to stop pipeline from going to TerraformApply
   # 2 = Succeeded with non-empty diff (changes present), need to continues pipeline to ApproveOrReject and TerraformApply
   # 1 = Error
-  # Try to enable the current pipeline's stage transition if error or No changes to allow the pipeline to resume after pausing for current execution
-  aws codepipeline enable-stage-transition \
-    --pipeline-name "$CODEPIPELINE_NAME" \
-    --stage-name Plan \
-    --transition-type Inbound
+  enable_stage_transition
+fi
+
+if [[ -n $TF_APPLY_EXIT_CODE ]]; then
+  enable_stage_transition
 fi
 
 post_plan_to_slack() {
