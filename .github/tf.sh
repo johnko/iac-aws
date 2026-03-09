71cce32182c12b352c7d5ef34d78cc3ef2797953
johnko <279736+johnko@users.noreply.github.com>
2026-03-09 06:23:04 -0400

    fix export

--- .github/tf.sh
+++ .github/tf.sh
@@ -62,6 +62,7 @@ fi
 if [[ -e shared_tfstate_backend.template ]]; then
   if [[ -z "$TF_VAR_workspace" ]]; then
     TF_VAR_workspace="$WORKSPACE"
+    export TF_VAR_workspace
   fi
   envsubst <shared_tfstate_backend.template >shared_tfstate_backend.tf
 fi
