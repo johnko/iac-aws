28c5af77795626c48bef5e2ddbf68a0153be4ed4
johnko <279736+johnko@users.noreply.github.com>
2026-03-09 02:02:24 -0400

    add s3 replication role for tfstate

--- aws/org1/foundation_terraform_state_s3_bucket.tf
+++ aws/org1/foundation_terraform_state_s3_bucket.tf
@@ -107,3 +107,71 @@ resource "aws_s3_bucket_request_payment_configuration" "terraform_state_replica"
   bucket = aws_s3_bucket.terraform_state_replica[each.key].id
   payer  = "Requester"
 }
+
+########################################
+
+resource "aws_iam_role" "S3ReplicationRole-tfstate" {
+  name = "S3ReplicationRole-tfstate"
+  assume_role_policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Effect" : "Allow",
+        "Principal" : {
+          "Service" : "s3.amazonaws.com"
+        },
+        "Action" : "sts:AssumeRole",
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy" "S3ReplicationRoleDefaultPolicy" {
+  name = "S3ReplicationRoleDefaultPolicy"
+  role = aws_iam_role.S3ReplicationRole-tfstate.id
+
+  policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Action" : [
+          "s3:GetReplicationConfiguration",
+          "s3:ListBucket",
+        ],
+        "Resource" : [
+          aws_s3_bucket.terraform_state.arn,
+        ],
+        "Effect" : "Allow"
+      },
+      {
+        "Action" : [
+          "s3:GetObjectVersionForReplication",
+          "s3:GetObjectVersionAcl",
+          "s3:GetObjectVersionTagging",
+        ],
+        "Resource" : [
+          "${aws_s3_bucket.terraform_state.arn}/*"
+        ],
+        "Effect" : "Allow"
+      },
+      {
+        "Action" : [
+          "s3:ReplicateObject",
+          "s3:ReplicateDelete",
+          "s3:ReplicateTags",
+        ],
+        "Resource" : flatten([
+          for k, v in aws_s3_bucket.terraform_state_replica : [
+            "${v.arn}/*"
+          ]
+        ]),
+        "Effect" : "Allow"
+      },
+    ]
+  })
+}
+
+resource "aws_iam_role_policies_exclusive" "S3ReplicationRole-tfstate" {
+  role_name    = aws_iam_role.S3ReplicationRole-tfstate.name
+  policy_names = [aws_iam_role_policy.S3ReplicationRoleDefaultPolicy.name]
+}
