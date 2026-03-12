ec76b49cf68c72baa4ae83048a157d74a2a00085
johnko <279736+johnko@users.noreply.github.com>
2026-03-12 06:39:36 -0400

    check for suffix

--- aws/org1/sandbox_bedrock/foundation/_import.sh
+++ aws/org1/sandbox_bedrock/foundation/_import.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 set -euxo pipefail
 
-terraform state show 'aws_s3_bucket.terraform_state' ||
+terraform state show 'aws_s3_bucket.terraform_state' | grep cacentral1 ||
   terraform state rm 'aws_s3_bucket.terraform_state'
