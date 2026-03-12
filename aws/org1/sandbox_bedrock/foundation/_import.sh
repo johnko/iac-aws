583b51d045e76b6c21ee857db2ca2daf8e5eddfa
johnko <279736+johnko@users.noreply.github.com>
2026-03-12 06:44:33 -0400

    abandon more for rename

--- aws/org1/sandbox_bedrock/foundation/_import.sh
+++ aws/org1/sandbox_bedrock/foundation/_import.sh
@@ -3,3 +3,18 @@ set -euxo pipefail
 
 terraform state show 'aws_s3_bucket.terraform_state' | grep cacentral1 ||
   terraform state rm 'aws_s3_bucket.terraform_state'
+
+terraform state show 'aws_s3_bucket_ownership_controls.terraform_state' | grep cacentral1 ||
+  terraform state rm 'aws_s3_bucket_ownership_controls.terraform_state'
+
+terraform state show 'aws_s3_bucket_public_access_block.terraform_state' | grep cacentral1 ||
+  terraform state rm 'aws_s3_bucket_public_access_block.terraform_state'
+
+terraform state show 'aws_s3_bucket_replication_configuration.terraform_state' | grep cacentral1 ||
+  terraform state rm 'aws_s3_bucket_replication_configuration.terraform_state'
+
+terraform state show 'aws_s3_bucket_request_payment_configuration.terraform_state' | grep cacentral1 ||
+  terraform state rm 'aws_s3_bucket_request_payment_configuration.terraform_state'
+
+terraform state show 'aws_s3_bucket_versioning.terraform_state' | grep cacentral1 ||
+  terraform state rm 'aws_s3_bucket_versioning.terraform_state'
