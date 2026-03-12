74cbfe4e7b7bdb6f8fe5e63773071aaff90e8006
johnko <279736+johnko@users.noreply.github.com>
2026-03-12 07:45:58 -0400

    remove import script

--- aws/org1/security_cloudtrail/foundation/_import.sh
+++ aws/org1/security_cloudtrail/foundation/_import.sh
@@ -1,20 +1,2 @@
 #!/usr/bin/env bash
 set -euxo pipefail
-
-terraform state show 'aws_s3_bucket.terraform_state' | grep cacentral1 ||
-  terraform state rm 'aws_s3_bucket.terraform_state' || true
-
-terraform state show 'aws_s3_bucket_ownership_controls.terraform_state' | grep cacentral1 ||
-  terraform state rm 'aws_s3_bucket_ownership_controls.terraform_state' || true
-
-terraform state show 'aws_s3_bucket_public_access_block.terraform_state' | grep cacentral1 ||
-  terraform state rm 'aws_s3_bucket_public_access_block.terraform_state' || true
-
-terraform state show 'aws_s3_bucket_request_payment_configuration.terraform_state' | grep cacentral1 ||
-  terraform state rm 'aws_s3_bucket_request_payment_configuration.terraform_state' || true
-
-terraform state show 'aws_s3_bucket_versioning.terraform_state' | grep cacentral1 ||
-  terraform state rm 'aws_s3_bucket_versioning.terraform_state' || true
-
-terraform state show 'aws_s3_bucket_replication_configuration.terraform_state_replica' | grep cacentral1 ||
-  terraform state rm 'aws_s3_bucket_replication_configuration.terraform_state_replica' || true
