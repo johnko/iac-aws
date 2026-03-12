54c8fba7f4fe75d035235f05f773d0646997c928
johnko <279736+johnko@users.noreply.github.com>
2026-03-12 06:47:45 -0400

    dont error on bad abandon

--- aws/org1/sandbox_bedrock/foundation/_import.sh
+++ aws/org1/sandbox_bedrock/foundation/_import.sh
@@ -2,19 +2,19 @@
 set -euxo pipefail
 
 terraform state show 'aws_s3_bucket.terraform_state' | grep cacentral1 ||
-  terraform state rm 'aws_s3_bucket.terraform_state'
+  terraform state rm 'aws_s3_bucket.terraform_state' || true
 
 terraform state show 'aws_s3_bucket_ownership_controls.terraform_state' | grep cacentral1 ||
-  terraform state rm 'aws_s3_bucket_ownership_controls.terraform_state'
+  terraform state rm 'aws_s3_bucket_ownership_controls.terraform_state' || true
 
 terraform state show 'aws_s3_bucket_public_access_block.terraform_state' | grep cacentral1 ||
-  terraform state rm 'aws_s3_bucket_public_access_block.terraform_state'
+  terraform state rm 'aws_s3_bucket_public_access_block.terraform_state' || true
 
 terraform state show 'aws_s3_bucket_replication_configuration.terraform_state' | grep cacentral1 ||
-  terraform state rm 'aws_s3_bucket_replication_configuration.terraform_state'
+  terraform state rm 'aws_s3_bucket_replication_configuration.terraform_state' || true
 
 terraform state show 'aws_s3_bucket_request_payment_configuration.terraform_state' | grep cacentral1 ||
-  terraform state rm 'aws_s3_bucket_request_payment_configuration.terraform_state'
+  terraform state rm 'aws_s3_bucket_request_payment_configuration.terraform_state' || true
 
 terraform state show 'aws_s3_bucket_versioning.terraform_state' | grep cacentral1 ||
-  terraform state rm 'aws_s3_bucket_versioning.terraform_state'
+  terraform state rm 'aws_s3_bucket_versioning.terraform_state' || true
