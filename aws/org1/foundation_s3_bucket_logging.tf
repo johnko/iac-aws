f0f372a92935c3ba0ae0391fafcf1cda90fdec52
johnko <279736+johnko@users.noreply.github.com>
2026-03-23 08:21:10 -0400

    loop through governedRegions

--- aws/org1/foundation_s3_bucket_logging.tf
+++ aws/org1/foundation_s3_bucket_logging.tf
@@ -1,12 +1,9 @@
 module "logging" {
   source = "../../../../modules/s3-logging"
 
-  for_each = merge(
-    {
-      local.tfstate_primary_region: {}
-    },
-    local.tfstate_replica_regions,
-  )
+  for_each = {
+    for v in local.governedRegions : v => {}
+  }
 
   bucket_full_name = format("logging-%s-%s-an", data.aws_caller_identity.current.account_id, each.key)
   bucket_namespace = "account-regional"
